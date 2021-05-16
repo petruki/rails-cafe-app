b963581f456533d79f19b3fa4cd6606884269fdc
petruki <31597636+petruki@users.noreply.github.com>
2021-05-16 14:54:49 -0700

    #3 - Included post/patch/delete tests - refactored fixtures naming

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,7 +7,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should delete a product - Can Delete" do
-    product = products(:three)
+    product = products(:can_delete)
     
     assert_difference("Product.count", -1) do
       delete product_url(product)
@@ -21,12 +21,28 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not delete a product - Reason: associated to a course" do
-    product = products(:one)
+    product = products(:spaghetti)
     
     assert_difference("Product.count", 0) do
       delete product_url(product)
     end
 
     assert_redirected_to products_path
+  end
+
+  test "should create a product" do
+    assert_difference("Product.count", +1) do
+      post "/products", params: { product: { name: "New Product" } }
+    end
+
+    # Validate DB
+    assert Product.where(name: "New Product").count == 1
+  end
+
+  test "should edit a product" do
+    product = products(:spaghetti)
+    assert_difference("Product.where(name: 'Edited Product').count", 1) do
+      patch "/products/#{product.id}", params: { product: { name: "Edited Product" } }
+    end
   end
 end
