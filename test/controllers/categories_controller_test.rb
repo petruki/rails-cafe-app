511c244039f545658b2a01a995aa933eecdc6f11
petruki <31597636+petruki@users.noreply.github.com>
2021-05-18 23:41:01 -0700

    #7 - Cateogry tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,37 +2,33 @@ require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get categories_index_url
+    get categories_path
     assert_response :success
   end
 
-  test "should get new" do
-    get categories_new_url
-    assert_response :success
+  test "should delete a category - Can Delete" do
+    category = categories(:can_delete)
+    
+    assert_difference("Category.count", -1) do
+      delete category_url(category)
+    end
+
+    assert_redirected_to categories_path
   end
 
-  test "should get create" do
-    get categories_create_url
-    assert_response :success
+  test "should create a category" do
+    assert_difference("Category.count", +1) do
+      post "/categories", params: { category: { name: "New category" } }
+    end
+
+    # Validate DB
+    assert Category.where(name: "New category").count == 1
   end
 
-  test "should get show" do
-    get categories_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get categories_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get categories_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get categories_destroy_url
-    assert_response :success
+  test "should edit a category" do
+    category = categories(:main_courses)
+    assert_difference("Category.where(name: 'Edited category').count", 1) do
+      patch "/categories/#{category.id}", params: { category: { name: "Edited category" } }
+    end
   end
 end
