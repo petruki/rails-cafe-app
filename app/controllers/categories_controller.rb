0e9c2d2f1806a3616d7791631c417f4a3d09ea40
petruki <31597636+petruki@users.noreply.github.com>
2021-05-18 23:34:05 -0700

    #7 - Implemented Category CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,45 @@
 class CategoriesController < ApplicationController
+  before_action :current_category, only: [:show, :edit, :update, :destroy]
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
-  end
+    category = Category.create(category_params)
 
-  def show
-  end
-
-  def edit
+    redirect_to categories_path(category)
   end
 
   def update
+    @category.update(category_params)
+
+    redirect_to categories_path(@category)
   end
 
   def destroy
+    @category.destroy
+
+    redirect_to categories_path
+  end
+
+  def category_in_use(category_id)
+    Course.where(category_id: category_id).count > 0
+  end
+
+  helper_method :category_in_use
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def current_category
+    @category = Category.find(params[:id])
   end
 end
