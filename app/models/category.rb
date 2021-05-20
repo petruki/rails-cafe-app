d5de7e74fb72fa7327f79f24f12e25abec90dcb0
petruki <31597636+petruki@users.noreply.github.com>
2021-05-19 18:33:24 -0700

    Removed unused model, fixed associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-    has_many :course_items
+    has_many :courses
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,4 @@
 class Course < ApplicationRecord
     has_many :pricing_units, dependent: :delete_all
-    has_many :pricing_portion, dependent: :delete_all
     has_one :category
 end
